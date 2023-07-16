b4989f20a8141301629ba253643145676071692e
Ибрагим Гарданов <ibragimgardanov@MacBook-Pro-Ibragim.local>
2023-07-17 00:32:41 +0300

    task

---
+++ task013/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число:");
+        int number = int.Parse(Console.ReadLine());
+        int thirdDigit = (number / 1) % 10;
+        if (number < 100)
+        {
+            Console.WriteLine("третьей цыфры нет.");
+        }
+        else
+        {
+            Console.WriteLine($"Третяя цифра числа: {thirdDigit}");
+        }
+    }
+}
---
+++ task015/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+using System;
+ class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите номер дня недели (1-7):");
+        int dayOfWeek = int.Parse(Console.ReadLine());
+         if (dayOfWeek >= 1 && dayOfWeek <= 7)
+        {
+            if (dayOfWeek == 6 || dayOfWeek == 7)
+            {
+                Console.WriteLine("Этот день является выходным.");
+            }
+            else
+            {
+                Console.WriteLine("Этот день не является выходным.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некорректный номер дня недели. Введите число от 1 до 7.");
+        }
+    }
+}
